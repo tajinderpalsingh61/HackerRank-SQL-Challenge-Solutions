095a8aae3b5ae043646d7ca9def9f7739bba732b
NeerajSharmaDataverse <136225999+NeerajSharmaDataverse@users.noreply.github.com>
2023-07-02 11:51:06 +1000

    Update Weather Observation Station 5.sql
    New solution added

--- Basic Select/Weather Observation Station 5.sql
+++ Basic Select/Weather Observation Station 5.sql
@@ -17,3 +17,17 @@ WHERE MIN_LEN = 1 OR MAX_LEN =1
 ORDER BY CITY
 ) WHERE ROWNUM<=2
 ;
+
+
+--Solution 2
+
+select CITY, length(CITY)
+from
+(
+  select 
+    CITY, 
+    row_number() over (order by length(CITY), CITY) as shortest_is_one,
+    row_number() over (order by length(CITY) desc, CITY) as longest_is_one
+  from STATION
+)
+where shortest_is_one = 1 or longest_is_one = 1;
