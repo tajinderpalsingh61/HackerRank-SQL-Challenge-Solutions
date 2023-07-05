9ecec8d00acf0ec6fae864127617e0639844d508
NeerajSharmaDataverse <136225999+NeerajSharmaDataverse@users.noreply.github.com>
2023-07-05 18:23:08 +1000

    Update Weather Observation Station 9.sql
    New solution added

--- Basic Select/Weather Observation Station 9.sql
+++ Basic Select/Weather Observation Station 9.sql
@@ -2,3 +2,7 @@
 
 select distinct city from station
 where substr(lower(nvl(city,'x')),1,1) not in ('a','e','i','o','u') ;
+
+--solution 2
+
+SELECT UNIQUE CITY FROM STATION WHERE UPPER(SUBSTR(CITY,1,1)) NOT IN ('A','E','I','O','U');
